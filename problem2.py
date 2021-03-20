8d3dd319a99d93799fe1be07978a256dc18fb803
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-03-20 15:53:10 -0700

    done

--- problem2.py
+++ problem2.py
@@ -13,17 +13,24 @@ assert hypotenuse(12,5,False) == 13
 assert hypotenuse(5,3,True) == 4
 (2 points)
 """
-def hypotenuse(a,b, values):
-    if values == True:
-        if a > b:
-            hypotenuse = (a ** 2 - b ** 2) ** 0.5
-        elif b > a:
-            hypotenuse = (b ** 2 - a ** 2) ** 0.5
-    elif values == False:
-        hypotenuse = (a ** 2 + b ** 2) ** 0.5
-        return hypotenuse
+import math
 
-assert hypotenuse(5,3,True) == 4
+def hypotenuse(a,b,c):
+    if c == True:
+        p = (a**2)-(b**2)
+        p2 = float(math.sqrt(p))
+        return p2
+    elif c == False:
+        return math.sqrt(a**2 + b**2)
+
+x = hypotenuse(5,3,True)
+print(int(x))
+
+y = hypotenuse(12,5,False)
+print(int(y))
+
+
+
 
 
 
