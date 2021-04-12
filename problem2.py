de809655f8067ac3330ba7103e12ec488bdf1e63
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-12 00:30:22 -0700

    done

--- problem2.py
+++ problem2.py
@@ -13,14 +13,21 @@ assert hypotenuse(12,5,False) == 13
 assert hypotenuse(5,3,True) == 4
 (2 points)
 """
-def hypotenuse(a, b, values):
-    if values == True:
-        if a > b:
-            hyp = (a ** 2 - b ** 2) ** 0.5
-        elif b > a:
-            hyp = (b ** 2 - a ** 2) ** 0.5
-    elif values == False:
-        hyp = (a ** 2 + b ** 2) **0.5
-    return hyp
+def hypotenuse(x,y,z):
+    if z == True:
+        if x > y:
+            hyp = x
+            side = y
+            miss = ((hyp**2)-(side**2))**(1/2)
+        elif y > x:
+            hyp = y
+            side = x
+            miss = ((hyp**2)-(side**2))**(1/2)
+    elif z == False:
+        miss = ((x**2)+(y**2))**(1/2)
+    if miss%1 == 0:
+        miss = int(miss)
+    return miss
 
-assert hypotenuse(5,3,True) == 4
+
+print(hypotenuse(12,5,False))
